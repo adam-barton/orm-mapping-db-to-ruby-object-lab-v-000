4e6ae93aa08845d5ec6992aa0c45d4fe872eddc9
Adam Barton <adam.barton333@gmail.com>
2018-09-25 04:24:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,6 +60,18 @@ class Student
     end
   end
 
+  def self.first_X_students_in_grade_10(num)
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = ?
+    LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql, 10, num).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
 
   def save
     sql = <<-SQL
