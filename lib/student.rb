7edb5c7df74bf8c7126cc6143e07c3432e399987
Adam Barton <adam.barton333@gmail.com>
2018-09-25 04:18:03 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,14 @@ class Student
   end
 
   def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = ?
+    LIMIT 1
+    SQL
 
+    DB[:conn].execute(sql, 9).map do |row|
+      self.new_from_db(row)
   end
 
   def save
